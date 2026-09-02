2447e0ed08c49af63d6f9c8660605ea53a6b8859
Sonu Purohit <pruohitashok685@gmail.com>
2026-09-02 19:45:56 +0530

    add github link

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
 import { FaGithub, FaInstagram, FaLinkedin, FaSquareXTwitter } from 'react-icons/fa6'
 
 function Footer() {
   return (
-    
+
     <footer className='py-4'>
-        <div id="outline" className="border-t-4 border-t-[#00FFFF] mt-6"></div>
+      <div id="outline" className="border-t-4 border-t-[#00FFFF] mt-6"></div>
 
-        <div>
-                <ul className='flex gap-6 text-4xl py-4 w-fit mx-auto'>
-                    <li className='duration-300 hover:scale-110 hover:text-[#00FFFF]'> <a href="#"  ><FaInstagram/> </a> </li>
-                    <li className='duration-300 hover:scale-110 hover:text-[#00FFFF]'> <a href="#"  ><FaGithub /> </a> </li>
-                    <li className='duration-300 hover:scale-110 hover:text-[#00FFFF]'> <a href="#"  ><FaLinkedin  /> </a> </li>
-                    <li className='duration-300 hover:scale-110 hover:text-[#00FFFF]'> <a href="#"  ><FaSquareXTwitter/> </a> </li>
-                </ul>
-        </div>
+      <div>
+        <ul className='flex gap-6 text-4xl py-4 w-fit mx-auto'>
+          <li className='duration-300 hover:scale-110 hover:text-[#00FFFF]'> <a href="#"  ><FaInstagram /> </a> </li>
+          <li className='duration-300 hover:scale-110 hover:text-[#00FFFF]'> <a href="https://github.com/sonu8875"
+            target="_blank"
+            rel="noopener noreferrer"  ><FaGithub /> </a> </li>
+          <li className='duration-300 hover:scale-110 hover:text-[#00FFFF]'> <a href="#"  ><FaLinkedin /> </a> </li>
+          <li className='duration-300 hover:scale-110 hover:text-[#00FFFF]'> <a href="#"  ><FaSquareXTwitter /> </a> </li>
+        </ul>
+      </div>
 
-        <div>
-            <p className='text-center custom-text-secondary custom-font-poppins font-semibold text-xl'> &lt; This Portfolio Made by Me 😃 &#47; &gt; </p>
-        </div>
+      <div>
+        <p className='text-center custom-text-secondary custom-font-poppins font-semibold text-xl'> &lt; This Portfolio Made by Me 😃 &#47; &gt; </p>
+      </div>
     </footer>
   )
 }
